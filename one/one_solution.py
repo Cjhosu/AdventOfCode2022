081443eced202e36a3c4cb2aa93c5657a1e8e3b1
cjhosu <cjhosu@gmail.com>
2022-12-02 15:02:46 -0500

    Working cheaty solution day 2

--- one/one_solution.py
+++ one/one_solution.py
@@ -21,7 +21,7 @@ max_cals = 0
 second_most_cals = 0
 third_most_cals = 0
 
-with open('one_inputt ') as file:
+with open('one_input') as file:
     for line in file:
         if line  != '\n':
             cal_list.append(int(line.strip()))
@@ -32,17 +32,11 @@ with open('one_inputt ') as file:
                 third_most_cals = second_most_cals
                 second_most_cals = max_cals
                 max_cals = new_cals
-                print(max_cals)
-                print(max_cals, second_most_cals, third_most_cals)
             elif new_cals > second_most_cals:
                 third_most_cals = second_most_cals
                 second_most_cals = new_cals
-                print(second_most_cals)
-                print(max_cals, second_most_cals, third_most_cals)
             elif new_cals > third_most_cals:
                 third_most_cals = new_cals
-                print(third_most_cals)
-                print(max_cals, second_most_cals, third_most_cals)
             else:
                 pass
             cal_list = []
@@ -50,22 +44,15 @@ if new_cals > max_cals:
     third_most_cals = second_most_cals
     second_most_cals = max_cals
     max_cals = new_cals
-    print(max_cals)
-    print(max_cals, second_most_cals, third_most_cals)
 elif new_cals > second_most_cals:
     third_most_cals = second_most_cals
     second_most_cals = new_cals
-    print(second_most_cals)
-    print(max_cals, second_most_cals, third_most_cals)
 elif new_cals > third_most_cals:
     third_most_cals = new_cals
-    print(third_most_cals)
-    print(max_cals, second_most_cals, third_most_cals)
 else:
     pass
 
 big_cals_list = [max_cals, second_most_cals, third_most_cals]
 
-print(big_cals_list)
-
 print(sum(big_cals_list))
+
---
+++ two/two_solution.py
@@ -0,0 +1,25 @@
+# part one
+match_list = []
+score_sheet = {"AX" : 4, "AY": 8, "AZ": 3, "BX": 1, "BY" : 5, "BZ" : 9, "CX" : 7, "CY": 2, "CZ": 6}
+score = 0
+
+with open('two_input') as file:
+    for line in file:
+        match_list.append(line.strip('\n'))
+
+match_list = [ i.replace(' ','') for i in match_list]
+
+for i in match_list:
+    score += score_sheet[i]
+
+print(score)    
+
+# part two
+
+altered_score_sheet = {"AX" : 3, "AY": 4, "AZ": 8, "BX": 1, "BY" : 5, "BZ" : 9, "CX" : 2, "CY": 6, "CZ": 7}
+score = 0
+
+for i in match_list:
+    score += altered_score_sheet[i]
+
+print(score)    
