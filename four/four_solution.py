2432180c922a872ad4eb6a23f9f8ec7c309bd8dd
Cjhosu <cjhosu@gmail.com>
2022-12-06 23:25:58 -0500

    Day four part 2

--- four/four_solution.py
+++ four/four_solution.py
@@ -25,3 +25,19 @@ print(counter)
 
 
 # part two
+
+ws = []
+counter = 0
+for pair in zone_pairs:
+    for section_range in pair:
+        x = section_range.replace('-', ' ')
+        x = x.split()
+        ws.append(x[0])
+        ws.append(x[1])
+    if int(ws[0]) in range(int(ws[2]), int(ws[3]) +1) or int(ws[1]) in range(int(ws[2]),int(ws[3])+1):
+        counter += 1
+    elif int(ws[2]) in range(int(ws[0]), int(ws[1]) +1) or int(ws[3]) in range(int(ws[0]),int(ws[1])+1):
+        counter += 1
+    ws = []
+
+print(counter)
