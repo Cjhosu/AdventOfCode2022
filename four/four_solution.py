6db14740b8a099f84177f3e64fe8ca95583aedbc
Cjhosu <cjhosu@gmail.com>
2022-12-06 23:05:10 -0500

    Remove Debug statements

--- four/four_solution.py
+++ four/four_solution.py
@@ -16,10 +16,8 @@ for pair in zone_pairs:
         ws.append(x[1])
     if int(ws[0]) >= int(ws[2]) and int(ws[1]) <= int(ws[3]):
         counter += 1
-        print(ws[0], ws[1], ws[2], ws[3], "cond 1")
     elif int(ws[0]) <= int(ws[2]) and int(ws[1]) >= int(ws[3]):
         counter += 1
-        print(ws[0], ws[1], ws[2], ws[3], "cond 2")
     ws = []
 
 print(counter)
