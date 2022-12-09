11970f9ab9cee539ad4a8d1dfbb2043f3f077c60
cjhosu <cjhosu@gmail.com>
2022-12-09 16:36:02 -0500

    Pre vacation commit

--- five/five_solution.py
+++ five/five_solution.py
@@ -13,6 +13,13 @@ with open('five_sample') as file:
                 pass
 
 print(stack_pos)
+number_of_stacks = int(stack_pos[len(stack_pos) - 3])
 
+print(number_of_stacks)
 with open('five_sample') as file:
     for line in file:
+        if not line.startswith("move"):
+            line = list(line)
+            for i in range(1, len(line)):
+                if line[i] != ' ' :
+                    print(i, line[i])
